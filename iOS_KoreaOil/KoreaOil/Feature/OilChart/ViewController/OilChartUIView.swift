ca9b1d072b999f8c671540954f2c9b8d21874410
윤형찬 <72960152+velmash@users.noreply.github.com>
2024-01-05 13:43:46 +0900

    :wrench: 차트 뷰 초기 Layout 구현

--- iOS_KoreaOil/KoreaOil/Feature/OilChart/ViewController/OilChartUIView.swift
+++ iOS_KoreaOil/KoreaOil/Feature/OilChart/ViewController/OilChartUIView.swift
@@ -12,22 +12,27 @@ struct OilChartUIView: View {
     @ObservedObject var viewModel = OilChartViewModel()
     
     var body: some View {
-//        List(viewModel.weekPriceData, id: \.self) { data in
-//            Text(data.price)
-//        }
-//        .onAppear {
-//            viewModel.getParam()
-//        }
-        
-        Chart {
-            ForEach(Array(viewModel.weekPriceData.enumerated()), id: \.element) { index, data in
-
-                LineMark(
-                    x: .value("날짜", index),
-                    y: .value("가격", Double(data.price) ?? 0)
-                )
-                .foregroundStyle(by: .value("Cate1", "휘발유"))
+        VStack {
+            
+            Text("지난 7일간 유가 추이")
+                .font(.system(size: 24))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding([.top, .leading], 15)
+                .padding(.bottom, 20)
+            
+            Chart {
+                ForEach(Array(viewModel.weekPriceData.enumerated()), id: \.element) { index, data in
+                    LineMark(
+                        x: .value("날짜", index),
+                        y: .value("가격", Double(data.price) ?? 0)
+                    )
+                    .foregroundStyle(by: .value("Cate1", "휘발유"))
+                }
             }
+            .frame(height: 300) // 차트의 높이를 조정
+            .padding() // 주변에 패딩 추가
+            
+            Spacer() // 하단 여백
         }
         .onAppear {
             viewModel.getParam()
