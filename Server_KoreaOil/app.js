18cd862f0255838d3c239f31029a9d8cdcd404f0
윤형찬 <72960152+velmash@users.noreply.github.com>
2024-02-19 13:02:27 +0900

    :wrench: Node.js 서버 MySQL 재연결 이슈로 localhost 종료되는 이슈 수정

--- Server_KoreaOil/app.js
+++ Server_KoreaOil/app.js
@@ -4,15 +4,15 @@
     Command: lsof -i tcp:80 kill -9 PID 28476
 */
 
-const mysql = require("mysql");
+const mysql = require("mysql2/promise");
 const dbconfig = require("./config/database.js");
-const connection = mysql.createConnection(dbconfig);
 
 const axios = require('axios').default;
-
 const express = require("express");
 const app = express();
 const port = 80;
+
+const pool = mysql.createPool(dbconfig);
 
 const areaRoutes = require("./routes/areaRoutes");
 
@@ -20,64 +20,49 @@ app.set('port', process.env.PORT || port);
 
 app.use('/', areaRoutes);
 
-app.get('/api/regions', (req, res) => {
-  const regionName = req.query.regionName;
-  const prodcd = req.query.prodcd;
-  const query = 'SELECT * FROM Regions WHERE REGION_NAME = ?';
+app.get('/api/regions', async (req, res) => {
+    const regionName = req.query.regionName;
+    const prodcd = req.query.prodcd;
+    const query = 'SELECT * FROM Regions WHERE REGION_NAME = ?';
 
-  connection.query(query, [regionName], (error, rows) => {
-    if (error) {
-      // 에러 처리
-      console.error('Error occurred: ', error);
-      res.status(500).json({code: 500, message: 'An error occurred'});
-    } else {
-      // 쿼리 결과 로깅 및 클라이언트에 응답 전송
-      console.log('Query result: ', rows);
+    try {
+        // 연결 풀에서 연결을 가져와 쿼리를 실행
+        const [rows] = await pool.query(query, [regionName]);
 
-      if (rows.length > 0) {
-        var regionCode = rows[0].REGION_CODE;
-        const apiUrl = `http://www.opinet.co.kr/api/oilPriceList.do?code=C231214009&out=json&area=${regionCode}`;
+        if (rows.length > 0) {
+            var regionCode = rows[0].REGION_CODE;
+            const apiUrl = `http://www.opinet.co.kr/api/oilPriceList.do?code=C231214009&out=json&area=${regionCode}`;
 
-        axios.get(apiUrl)
-            .then(function (response) {
-              const filteredOilPrices = response.data.RESULT.OIL
-                  .filter(oilData => oilData.PRODCD === prodcd) // prodcd가 일치하는 항목만 필터링
-                  .sort((a, b) => a.PRICE - b.PRICE)
-                  .slice(0, 10) // 최대 10개 항목 선택
-                  .map(oilData => ({
+            const response = await axios.get(apiUrl);
+            const filteredOilPrices = response.data.RESULT.OIL
+                .filter(oilData => oilData.PRODCD === prodcd)
+                .sort((a, b) => a.PRICE - b.PRICE)
+                .slice(0, 10)
+                .map(oilData => ({
                     stationId: oilData.UNI_ID,
                     prodcd: oilData.PRODCD,
                     price: oilData.PRICE,
                     date: oilData.TRADE_DT,
                     time: oilData.TRADE_TM
-                  }));
+                }));
 
-              // 상세 정보 요청을 위한 모든 프로미스를 생성
-              const detailPromises = filteredOilPrices.map(oilPrice => {
+            const details = await Promise.all(filteredOilPrices.map(async oilPrice => {
                 const stationDetailURL = `http://www.opinet.co.kr/api/detailById.do?code=C231214009&out=json&id=${oilPrice.stationId}`;
-                return axios.get(stationDetailURL).then(response => response.data.RESULT); // API 응답의 데이터 반환
-              });
+                const detailResponse = await axios.get(stationDetailURL);
+                return detailResponse.data.RESULT;
+            }));
 
-              // 모든 프로미스가 완료되기를 기다림
-              Promise.all(detailPromises)
-                  .then(allDetails => {
-                    // 모든 상세 정보가 담긴 배열로 응답
-                    console.log(allDetails)
-                    res.json({ details: allDetails });
-                  })
-                  .catch(error => {
-                    console.error('Error fetching station details: ', error);
-                    res.status(500).json({ message: 'Error fetching station details' });
-                  });
-            });
-      } else {
-        // 쿼리 결과가 없는 경우, 404 상태 코드와 메시지 전송
-        res.status(404).json({code: 404, message: 'Region not found'});
-      }
+            console.log(details);
+            res.json({ details });
+        } else {
+            res.status(404).json({code: 404, message: 'Region not found'});
+        }
+    } catch (error) {
+        console.error('Error occurred: ', error);
+        res.status(500).json({code: 500, message: 'An error occurred'});
     }
-  });
 });
 
 app.listen(app.get('port'), () => {
-  console.log(`Server is running on http://localhost:${app.get('port')}`);
+    console.log(`Server is running on http://localhost:${app.get('port')}`);
 });
